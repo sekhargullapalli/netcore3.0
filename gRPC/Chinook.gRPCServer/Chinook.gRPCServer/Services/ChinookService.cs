6aa6e52beeb9a7671540775a410a3f7cdbbabf46
Vijaya Sekhar Gullapalli <sekhargullapalli@gmail.com>
2019-07-21 18:44:34 +0200

    extensions and empty services implemented

--- gRPC/Chinook.gRPCServer/Chinook.gRPCServer/Services/ChinookService.cs
+++ gRPC/Chinook.gRPCServer/Chinook.gRPCServer/Services/ChinookService.cs
@@ -27,5 +27,61 @@ namespace Chinook.gRPCServer
                 });
             return Task.FromResult(artists);
         }
+        public override Task<MediaTypes> GetAllMediaTypes(Empty request, ServerCallContext context)
+        {
+            MediaTypes mtypes = new MediaTypes();
+            foreach (var mtype in datacontext.MediaType)
+                mtypes.MediaTypeCollection.Add(new MediaTypes.Types.MediaType()
+                {
+                    MediaTypeID = (int)mtype.MediaTypeId,
+                    Name = mtype.Name
+                });
+            return Task.FromResult(mtypes);
+        }
+        public override Task<Genres> GetAllGenres(Empty request, ServerCallContext context)
+        {
+            Genres genres = new Genres();
+            foreach (var genre in datacontext.Genre)
+                genres.GenreCollection.Add(new Genres.Types.Genre()
+                {
+                    GenreID = (int)genre.GenreId,
+                    Name = genre.Name
+                });
+            return Task.FromResult(genres);
+        }
+        public override Task<Albums> GetAllAlbums(Empty request, ServerCallContext context)
+        {
+            Albums albums = new Albums();
+            foreach (var album in datacontext.Album)
+                albums.AlbumCollection.Add(new Albums.Types.Album()
+                {
+                    AlbumID = (int)album.AlbumId,
+                    Title = album.Title,
+                    Artist = new Artists.Types.Artist()
+                    {
+                        ArtistID = (int)album.Artist.ArtistId,
+                        Name=album.Artist.Name
+                    }
+                });
+            return Task.FromResult(albums);
+        }
+        public override Task<StoreEmployees> GetAllEmployees(Empty request, ServerCallContext context)
+        {
+            StoreEmployees employees = new StoreEmployees();
+            foreach (var employee in datacontext.Employee)
+                employees.EmployeeCollection.Add(new StoreEmployees.Types.StoreEmployee()
+                {
+                    FirstName = employee.FirstName,
+                    LastName=employee.LastName,
+                    HireDate = Timestamp.FromDateTime(employee.HireDate.ConvertToDateTime()),
+                    BirthDay = Timestamp.FromDateTime(employee.BirthDate.ConvertToDateTime()),
+                });
+            return Task.FromResult(employees);
+        }
+
+
+
+
+
     }
 }
---
+++ gRPC/Chinook.gRPCServer/Chinook.gRPCServer/Utilities/ChinookExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chinook.gRPCServer
+{
+    public static class ChinookExtensions
+    {
+        public static DateTime ConvertToDateTime (this byte[] bytes) =>
+            DateTime.Parse(System.Text.Encoding.UTF8.GetString(bytes));
+    }
+}
