e7bb8cee51e09ed55024292af479bd3cd344ea85
Vijaya Sekhar Gullapalli <vijaya.sekhargullapalli@swep.net>
2019-07-24 10:38:42 +0200

    blazor app initiated

---
+++ Blazor/CIAFactbook18App/CIAFactbook18App.DataSeeding/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CIAFactbook18App.DataSeeding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
