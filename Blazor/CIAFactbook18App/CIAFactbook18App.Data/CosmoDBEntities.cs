3e37348425a28308bd81be73d87b3b91b26f052c
Vijaya Sekhar Gullapalli <vijaya.sekhargullapalli@swep.net>
2019-07-24 11:21:40 +0200

    blazor project and entity classes added

---
+++ Blazor/CIAFactbook18App/CIAFactbook18App.Data/CosmoDBEntities.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace CIAFactbook18App.Data
+{
+    //Entity classes for cosmos db
+
+    public class ComparableField
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+        public string FieldName { get; set; }
+        public string Category { get; set; }
+        public bool IsDescending { get; set; }
+    }
+    public class FieldDefinition
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+        public string FieldName { get; set; }
+        public string Definition { get; set; }
+    }
+    public class Country
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string GEC { get; set; }
+        public string ISO_3166_1_Alpha2 { get; set; }
+        public string ISO_3166_1_Alpha3 { get; set; }
+        public string ISO_3166_1_Numeric { get; set; }
+        public string STANAG { get; set; }
+        public string Internet { get; set; }
+        public string Comment { get; set; }
+        public string Flagfile { get; set; }
+        public string AnthemFile { get; set; }
+        public string Datafile { get; set; }
+    }
+
+    
+}
---
+++ Blazor/CIAFactbook18App/CIAFactbook18App.Data/ProfileEntity.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Collections.Generic;
+
+namespace CIAFactbook18App.Data
+{
+    public enum ProfileEntityType { None, Category, Field, SubField }
+
+    /// <summary>
+    /// A profile entity object is used to hold the details of a given county in a recursive manner
+    /// </summary>
+    public class ProfileEntity
+    {
+        [JsonConverter(typeof(StringEnumConverter))]
+        public ProfileEntityType EntityType { get; set; } = ProfileEntityType.None;
+        public string Key { get; set; } = "";
+        public string Value { get; set; } = "";
+        public string Note { get; set; } = "";
+        public string Date { get; set; } = "";
+
+        public bool IsHistoricEntity { get; set; } = false;
+        public bool IsNumericEntity { get; set; } = false;
+        public bool IsGroupedEntity { get; set; } = false;
+
+        public int? ComparisonRank { get; set; } = null;
+
+        public List<ProfileEntity> Children { get; set; } = new List<ProfileEntity>();
+    }
+}
