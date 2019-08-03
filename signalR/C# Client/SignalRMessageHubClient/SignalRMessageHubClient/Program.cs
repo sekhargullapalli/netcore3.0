a7d7c8ce79f2035f7e0fffe7115988d195153e8c
Vijaya Sekhar Gullapalli <sekhargullapalli@gmail.com>
2019-08-03 16:05:12 +0200

    c# client added

---
+++ signalR/C# Client/SignalRMessageHubClient/SignalRMessageHubClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR.Client;
+using System.Threading.Tasks;
+
+namespace SignalRMessageHubClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Use your signalr app url here
+            var connection = new HubConnectionBuilder()
+                            .WithUrl("https://localhost:44391/messagehub")
+                            .Build();
+
+            connection.On<string, string>("SRMessage", (sender, message) => {
+                Console.WriteLine($"{sender} says {message}");
+            });
+            await connection.StartAsync();
+            await connection.InvokeAsync("SendMessage", $"C# Client", "C# Client Started..").ContinueWith(task =>
+            {
+                if (task.IsFaulted)
+                {
+                    Console.WriteLine("There was an error calling send: {0}",
+                                      task.Exception.GetBaseException());
+                }
+                else
+                {
+                    Console.WriteLine(task.Status);
+                }
+            });
+            Console.Read();
+            await connection.StopAsync();
+        }
+    }
+}
